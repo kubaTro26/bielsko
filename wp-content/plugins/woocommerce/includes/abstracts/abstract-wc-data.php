3ee6bb0b0d7f3e14d85548e114c55163d8586225
kuba <Jakub Troczynski>
2022-10-21 12:37:19 +0200

    commit

--- wp-content/plugins/woocommerce/includes/abstracts/abstract-wc-data.php
+++ wp-content/plugins/woocommerce/includes/abstracts/abstract-wc-data.php
@@ -330,7 +330,9 @@ abstract class WC_Data {
 
 		//dumping
 
-		echo 'getting123';
+		echo '<div>';
+		var_dump(debug_backtrace());
+		echo '</div>';
 		if ( $this->is_internal_meta_key( $key ) ) {
 			$function = 'get_' . ltrim( $key, '_' );
 
