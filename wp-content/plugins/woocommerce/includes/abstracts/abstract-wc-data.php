46e9ad2a94f4864db5bc2c22db57ebc243d01992
kuba <Jakub Troczynski>
2022-10-22 15:18:11 +0200

    commit

--- wp-content/plugins/woocommerce/includes/abstracts/abstract-wc-data.php
+++ wp-content/plugins/woocommerce/includes/abstracts/abstract-wc-data.php
@@ -621,10 +621,11 @@ abstract class WC_Data {
 		//var_dump($cache_loaded);
 
 
-		var_dump($raw_meta_data);
+		
 
 		if ( is_array( $raw_meta_data ) ) {
 			foreach ( $raw_meta_data as $meta ) {
+				var_dump($meta);
 				$this->meta_data[] = new WC_Meta_Data(
 					array(
 						'id'    => (int) $meta->meta_id,
