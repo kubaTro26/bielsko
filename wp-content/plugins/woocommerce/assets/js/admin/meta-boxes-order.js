7f159c01497ca84ab97972491224db2b605ef674
kuba <Jakub Troczynski>
2022-10-25 12:05:20 +0200

    commit

--- wp-content/plugins/woocommerce/assets/js/admin/meta-boxes-order.js
+++ wp-content/plugins/woocommerce/assets/js/admin/meta-boxes-order.js
@@ -231,6 +231,7 @@ jQuery( function ( $ ) {
 						 $('#_billing_postcode').val(response.meta_data[4].value);
 						 $('#_billing_address_2').val(response.meta_data[5].value);
 						 $('#_billing_address_1').val(response.meta_data[6].value);
+						 $('#_billing_email').val(response.meta_data[7].value);
 						 
 						 
 
