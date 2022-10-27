d8f1064609e4805a0b0a653c0c991565f74042ed
kuba <Jakub Troczynski>
2022-10-27 13:44:38 +0200

    commit

--- wp-content/plugins/woocommerce/includes/data-stores/class-wc-customer-data-store.php
+++ wp-content/plugins/woocommerce/includes/data-stores/class-wc-customer-data-store.php
@@ -535,7 +535,7 @@ class WC_Customer_Data_Store extends WC_Data_Store_WP implements WC_Customer_Dat
 			)
 		);
 
-		var_dump($query);
+		
 		//dumping7
 		
 
@@ -570,7 +570,7 @@ class WC_Customer_Data_Store extends WC_Data_Store_WP implements WC_Customer_Dat
 		if ( $limit && count( $results ) > $limit ) {
 			$results = array_slice( $results, 0, $limit );
 		}
-
+		var_dump($results);
 		return $results;
 	}
 
