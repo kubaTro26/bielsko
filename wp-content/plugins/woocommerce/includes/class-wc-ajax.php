5f88099a32a5e9b7ad36c27a7650c45e05aa2ed2
kuba <Jakub Troczynski>
2022-10-27 15:23:04 +0200

    commit

--- wp-content/plugins/woocommerce/includes/class-wc-ajax.php
+++ wp-content/plugins/woocommerce/includes/class-wc-ajax.php
@@ -1692,7 +1692,7 @@ class WC_AJAX {
 				$customer->get_email()
 			);
 		}
-		var_dump($customer->get_first_name());
+		var_dump($customer->get_id());
 		
 		wp_send_json( apply_filters( 'woocommerce_json_search_found_customers', $found_customers ) );
 	}
