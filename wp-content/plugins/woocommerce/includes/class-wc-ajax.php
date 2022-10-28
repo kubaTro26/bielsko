92697d6298aa8a1611c2ec3b391af998d1d38a92
kuba <Jakub Troczynski>
2022-10-28 10:19:00 +0200

    commit

--- wp-content/plugins/woocommerce/includes/class-wc-ajax.php
+++ wp-content/plugins/woocommerce/includes/class-wc-ajax.php
@@ -1704,10 +1704,13 @@ class WC_AJAX {
 			
 
 			);
+
+			var_dump($found_customers[ $id ]);
 		}
 		
 		
-		//wp_send_json( apply_filters( 'woocommerce_json_search_found_customers', $found_customers ) );
+		
+		wp_send_json( apply_filters( 'woocommerce_json_search_found_customers', $found_customers ) );
 	}
 
 	/**
