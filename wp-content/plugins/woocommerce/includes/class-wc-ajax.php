0e71cd7fe31a1557c7431c9c5d4ba2c19523089a
kuba <Jakub Troczynski>
2022-10-28 10:20:12 +0200

    commit

--- wp-content/plugins/woocommerce/includes/class-wc-ajax.php
+++ wp-content/plugins/woocommerce/includes/class-wc-ajax.php
@@ -1693,10 +1693,12 @@ class WC_AJAX {
 
 				}
 			}
+
+			var_dump($billing_firstname);
 			/* translators: 1: user display name 2: user ID 3: user email */
 			$found_customers[ $id ] = sprintf(
 				/* translators: $1: customer name, $2 customer id, $3: customer email */
-				esc_html__( '%1$s %4$s (#%2$s &ndash; %3$s)', 'woocommerce' ),
+				esc_html__( '%1$s  (#%2$s &ndash; %3$s) %4$s', 'woocommerce' ),
 				$customer->get_first_name() . ' ' . $customer->get_last_name(),
 				$customer->get_id(),
 				$customer->get_email(),
