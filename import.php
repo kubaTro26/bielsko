96578308ccc314094c5ff2ac2c55759ebdcdb7af
kuba <Jakub Troczynski>
2022-11-04 12:29:32 +0100

    commit

--- import.php
+++ import.php
@@ -53,7 +53,7 @@ $matches=[];
 
 
 //preg_match('/(foo)(bar)(baz)/', 'foobarbaz', $matches, PREG_OFFSET_CAPTURE);
-preg_match('/.*/',$content, &$matches );
+preg_match('/.*/',$content, $matches );
 
 
 var_dump($matches);
