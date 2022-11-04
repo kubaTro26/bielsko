954cf647f1283bff7c8d8271883422ab51afd709
kuba <Jakub Troczynski>
2022-11-04 04:15:19 +0100

    commit

--- wp-content/plugins/woocommerce/includes/admin/class-wc-admin-profile.php
+++ wp-content/plugins/woocommerce/includes/admin/class-wc-admin-profile.php
@@ -181,6 +181,9 @@ if ( ! class_exists( 'WC_Admin_Profile', false ) ) :
 
 			$show_fields = $this->get_customer_meta_fields();
 
+
+
+			echo 'action1';
 			var_dump( $GLOBALS['wp_actions']);
 			// add_action( 'admin_init', 'my_plugin_assets2' );
 			// function my_plugin_assets2() {
