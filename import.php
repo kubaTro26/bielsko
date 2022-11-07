924ba574c8452bc5d77e8d0af4a961ab66fdb236
kuba <Jakub Troczynski>
2022-11-07 21:57:03 +0100

    commit

--- import.php
+++ import.php
@@ -351,9 +351,9 @@ foreach ($matched as $ma){
         }
 
 
-        if($second[$o][11][0]!=null){
+        if($second[$o][12][0]!=null){
 
-          $object->telefon=$second[$o][11][0][0];
+          $object->telefon=$second[$o][12][0][0];
 
          }
          else{
