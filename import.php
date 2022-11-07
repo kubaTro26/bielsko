8d17bcf9b2c0e8770124de61068d0bbcb33657c9
kuba <Jakub Troczynski>
2022-11-07 20:53:31 +0100

    commit

--- import.php
+++ import.php
@@ -373,13 +373,13 @@ foreach ($matched as $ma){
 
 
      
-        
+        var_dump($object->user_nicename);
   
     
 
       
        
-        
+
         
         // $sql = "INSERT INTO `wp_users` (`ID`, `user_login`, `user_pass`, `user_nicename`, 
         // `user_email`, `user_url`, `user_registered`, `user_activation_key`, `user_status`, 
