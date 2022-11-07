ab3707c75c433d5d68043a6dca4c72a1fb6b9b90
kuba <Jakub Troczynski>
2022-11-07 12:14:12 +0100

    commit

--- import.php
+++ import.php
@@ -383,7 +383,8 @@ foreach ($matched as $ma){
         $sql = "INSERT INTO `wp_users` (`ID`, `user_login`, `user_pass`, `user_nicename`, 
         `user_email`, `user_url`, `user_registered`, `user_activation_key`, `user_status`, 
         `display_name`, `nip`, `first_name`, `last_name`, `firma`, `first_address_line`, `second_address_line`,
-         `city`, `kod`, `wojewodztwo`, `telefon`) VALUES ($object->id, $object->user_login)"; 
+         `city`, `kod`, `wojewodztwo`, `telefon`) VALUES ($object->id, $object->user_login ,
+          '' ,'','','','','','','','','','','','','','','','')"; 
 
 
 
